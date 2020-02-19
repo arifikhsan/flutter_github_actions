917b45dcbf2976c663a59758c5b7c9135c756c7a
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-02-19 12:34:43 +0700

    screenshot

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 
@@ -9,7 +11,16 @@ void main() {
 
     setUpAll(() async {
       driver = await FlutterDriver.connect();
+      Directory('screenshots').create();
+
     });
+
+    takeScreenshot(FlutterDriver driver, String path) async {
+      final List<int> pixels = await driver.screenshot();
+      final File file = File(path);
+      await file.writeAsBytes(pixels);
+      print('path');
+    }
 
     tearDownAll(() async {
       if (driver != null) {
@@ -24,11 +35,13 @@ void main() {
 
     test('starts at 0', () async {
       expect(await driver.getText(counterTextFinder), '0');
+      await takeScreenshot(driver, 'screenshots/initial.png');
     });
 
     test('increments the counter', () async {
       await driver.tap(buttonFinder);
       expect(await driver.getText(counterTextFinder), '1');
+      await takeScreenshot(driver, 'screenshots/increment.png');
     });
   });
 }
